89436638e94812ce0ec025a75ff8fdca3bea41ba
Juan David Granados <jd.granados@uniandes.edu.co>
2019-06-11 08:58:15 -0500

    creacion basica de estructura del proyecto

---
+++ recomendaciones/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Torre</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>One</v-btn>
+        <v-btn flat>Two</v-btn>
+        <v-btn flat>Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
